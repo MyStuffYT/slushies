5c95ad6b7e879cd0275fc39c094aceb9f33bf212
MyStuffYT <zoufirsamid@gmail.com>
2025-08-03 20:21:43 +0100

    homepage partially

---
+++ main.py
@@ -0,0 +1,34 @@
+# made w/ ai (actual intelligence)
+
+# online tools used:
+#
+# Flask documentation
+
+import os
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def thing():
+    return render_template("index.html")
+
+#https://flask.palletsprojects.com/en/stable/patterns/favicon/
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/style.css')
+def stylesheet():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'style.css', mimetype='text/css')
+
+@app.route('/logo.png')
+def logo():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'logo.png', mimetype='image/png')
+
+
+app.run(host='0.0.0.0', debug=True)

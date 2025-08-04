ecaa435ac2f582b09ce2ab896d2e74e08e6de296
MyStuffYT <zoufirsamid@gmail.com>
2025-08-04 02:04:33 +0100

    we are SO fucking done!! full proj

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 
 import os
 from flask import Flask, render_template, send_from_directory
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -34,3 +36,11 @@ def logo():
 def menu():
     return render_template('menu.html')
 
+@app.route("/buy") #yay last endpoint!?
+def buy():
+    return render_template("payment.html")
+
+@app.route("/catfact")
+def catfact():
+    catfactdict = requests.get("https://catfact.ninja/fact?max_length=45").json()
+    return catfactdict["fact"]
